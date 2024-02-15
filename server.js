fda23b4c51f4e10b78ed47f34ff18b49c905e5cc
ecostabal <ecostabal@urbex.cl>
2024-02-15 17:52:53 -0300

    secondcommit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta para tu aplicación
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Manejo de errores
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
